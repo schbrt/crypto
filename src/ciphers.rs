088ef9e22ee5cc72b3cf1412c92af2b8848b7884
schbrt <schubert.michael.j@gmail.com>
2020-07-12 15:01:38 -0500

    added vigenere decipher function

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
+//TODO: Historical ciphers would remove all capitals, spaces, and punctuation. The
+//output would be formatted in 4 or 5 letter blocks.
+
 // Implements a simple caesar cipher. Takes an input message and shifts each letter
 // by the specified amount. Only encodes a-zA-Z.
 // TODO: Implement flag for including capital letters.
@@ -42,6 +45,7 @@ pub mod caesar {
 }
 
 //TODO: Find better way than char.to_string()
+//TODO: Fix error with caps
 pub mod vigenere {
     pub fn encipher(msg: &str, key: &str) -> String {
         let mut key_iter = key.chars().cycle();
@@ -59,6 +63,17 @@ pub mod vigenere {
                     }}}).collect();
         return out;
     }
+
+    // Shifts each char in the key to the corresponding
+    // value to reverse.
+    pub fn decipher(msg: &str, key: &str) -> String {
+        let d_key: String = key.chars().map(|c| {
+            // This likely allocates case for each char?
+            let case = if c.is_uppercase() { b'A' } else { b'a' };
+            (26 - (c as u8 - case) + case) as char
+        }).collect();
+        encipher(msg, &d_key)
+    }
 }
 
 // Calculate the kl divergence for two distributions. Lower means
@@ -72,6 +87,7 @@ fn kullback_leibler(dist1: Vec<f32>, dist2: Vec<f32>) -> f32 {
     }).sum()
 }
 
+// Handling caps would better reflect the underlying structure of language.
 fn get_character_frequencies(msg: &str) -> Vec<f32> {
     let mut counter = HashMap::new();
     let mut num_chars = 0.0;
@@ -103,7 +119,7 @@ mod tests {
         assert_eq!(super::caesar::encipher("abc", 1), "bcd");
         assert_eq!(super::caesar::encipher("a b c", 1), "b c d");
         assert_eq!(super::caesar::encipher("abc", 27), "bcd");
-        assert_eq!(super::caesar::encipher("Alan Turing", 13), "Nyna Ghevat")
+        assert_eq!(super::caesar::encipher("Alan Turing", 13), "Nyna Ghevat");
         }
 
     #[test]
@@ -113,7 +129,9 @@ mod tests {
     }
 
     #[test]
-    fn test_vigenere_encipher() {
-        assert_eq!(super::vigenere::encipher("attackatdawn", "lemon"), "lxfopvefrnhr")
+    fn test_vigenere() {
+        assert_eq!(super::vigenere::encipher("attackatdawn", "lemon"), "lxfopvefrnhr");
+//        assert_eq!(super::vigenere::encipher("Attackatdawn", "lemon"), "Lxfopvefrnhr");
+        assert_eq!(super::vigenere::decipher("lxfopvefrnhr", "lemon"), "attackatdawn")
     }
 }
